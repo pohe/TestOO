770854ea57fd253a988f146bc4e592749c72eb1b
Poul Henriksen <POHE@zealand.dk>
2023-09-18 11:06:30 +0200

    gkj

--- TestOO/Program.cs
+++ TestOO/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using TestOO;
+
+Student s1 = new Student("Ole", 25, "Datamatiker", "12121212", 1); //argumenter
+Student s2 = new Student("Peter", 27, "Laborant", "13131313", 3);
+
+s1.PrintInfo();
+s2.PrintInfo();
+
+//string n = s1.Name; //Kalder get delen på propertien
+
+s1.Name = "Susanne"; //kalder set delen på propertien
+
+Console.WriteLine($"Student navn { s1.Name } skal betale { s1.CostPrPrint()   }  ");
+Console.WriteLine($"Student navn { s2.Name } skal betale { s2.CostPrPrint()  }  ");
---
+++ TestOO/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOO
+{
+    public class Student
+    {
+
+        //Instance fields - gemmes data sålænge objektet eksisterer
+        private string _name;
+        private int _age;
+        private string _education;
+        private string _phone;
+        private int _semester;
+
+
+        private const int basicPrintPrice = 300; 
+
+        //Property, bruges til at tilgå instance fields
+
+        public string Name
+        {
+            get { return _name; }
+            set {              
+                _name = value; 
+            }
+        }
+
+
+
+
+        //Constructor - bruges til at skabe og initialisere et objekt
+        //har ingen returtype
+        public Student(string name, int age, string education, string phone, int semester)
+        {
+            _name = name;
+            _age = age;
+            _education = education;
+            _phone = phone;
+            _semester = semester;
+        }
+
+
+        //Methods
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Student navn {_name} alder {_age} uddannelse {_education} tel {_phone} semester {_semester}");
+        }
+
+        public double CostPrPrint()
+        {
+            return  _semester * basicPrintPrice;
+        }
+
+
+    }
+}
